d7e8c4e01542de43c5610eceafffee4f500e9af3
dubenju <dubenju@gmail.com>
2020-05-24 22:38:38 +0900

    jSignature

---
+++ demo/src/main/java/com/example/demo/controller/SignController.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SignController {
+    @RequestMapping("/sign")
+    public String sign() {
+        return "user/sign";
+    }
+}
--- demo/src/main/java/com/example/demo/controller/User2Controller.java
+++ demo/src/main/java/com/example/demo/controller/User2Controller.java
@@ -1,18 +1,18 @@
-package com.example.demo.controller;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.example.demo.entity.User;
-import com.example.demo.repository.UserRepository;
-
-@RestController
-public class User2Controller {
-    @Autowired
-    private UserRepository userRepository;
-
-    @GetMapping("/init")
-    private void init() throws Exception {
-        userRepository.save(new User("taro"));
-    }
-}
+//package com.example.demo.controller;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import com.example.demo.entity.User;
+//import com.example.demo.repository.UserRepository;
+//
+//@RestController
+//public class User2Controller {
+//    @Autowired
+//    private UserRepository userRepository;
+//
+//    @GetMapping("/init")
+//    private void init() throws Exception {
+//        userRepository.save(new User("taro"));
+//    }
+//}
